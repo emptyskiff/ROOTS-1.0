df050166bb8dae719eb7c9bc5b9bb1b9ab00882d
emptyskiff <43298912+emptyskiff@users.noreply.github.com>
2021-03-07 16:55:35 +0300

    07/03/21
    Camera breathing
    Ready to introduce sound

---
+++ Assets/Scripts/Try/CameraRect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRect : MonoBehaviour
+{
+    private Camera cam;
+    float startX = 0;
+    float desiredX;
+    float startWidth = 1;
+    float desiredWidth;
+    public float alpha = 0;
+
+    public bool changeAspect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        desiredWidth = 1 / cam.aspect;
+        desiredX = (1 - desiredWidth) / 2;
+        changeAspect = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changeAspect)
+        {
+            float x = Mathf.Lerp(startX, desiredX, alpha);
+            float width = Mathf.Lerp(startWidth, desiredWidth, alpha);
+            cam.rect = new Rect(x, 0, width, 1);
+        }
+    }
+
+    public void ChangeAspect()
+    {
+        changeAspect = true;
+        GetComponentInChildren<Animator>().Play("RectChange");
+    }
+}
